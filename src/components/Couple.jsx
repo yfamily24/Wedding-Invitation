1fb76e76c9f6b634eaee5b81c8d8dcbd759d378b
Ilham Ramadhan <ilham_r672@mitrais.com>
2026-06-19 10:55:05 +0700

    Add gallery section, add welcome photo, groom and bride photo

--- src/components/Couple.jsx
+++ src/components/Couple.jsx
@@ -1,9 +1,10 @@
 import { config } from '../config'
 
-function Person({ role, person }) {
+function Person({ role, person, photo }) {
   return (
     <div className="person reveal">
       <p className="role">{role}</p>
+      <img className="person-photo" src={`assets/${photo}`} alt={person.name} />
       <h2 className="script pname">{person.name}</h2>
       <p className="parents">
         {role === 'The Groom' ? 'Putra dari' : 'Putri dari'}
@@ -23,9 +24,9 @@ export default function Couple() {
         kami bermaksud menyelenggarakan pernikahan putra-putri kami
       </p>
 
-      <Person role="The Groom" person={config.groom} />
+      <Person role="The Groom" person={config.groom} photo="groom.jpg" />
       <div className="amp reveal">&amp;</div>
-      <Person role="The Bride" person={config.bride} />
+      <Person role="The Bride" person={config.bride} photo="bride.jpg" />
     </section>
   )
 }
